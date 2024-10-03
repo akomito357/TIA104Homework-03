affc071a4110cc50c16b83c08d2f2a343ace3ba8
Ariel Chien <jacinth170625@gmail.com>
2024-10-03 20:04:02 +0800

    modify Lotto.java to advanced version

--- src/hw3/Lotto2.java
+++ src/hw3/Lotto2.java
@@ -13,14 +13,14 @@ public class Lotto2 {
 	
 	public Lotto2() {
 		ngNumber = getNumber();
+		numberList = new int[45];
 	}
 
 	public static void main(String[] args) {
 		Lotto2 lotto = new Lotto2();
 //		lotto.ngNumber = lotto.getNumber();
 		lotto.getNumberList(lotto.ngNumber);
-		
-		
+		lotto.printBetNumber(lotto.numberList);
 	}
 	
 	public int getNumber() {
@@ -35,6 +35,7 @@ public class Lotto2 {
 		for (int i = 1; i <= 49; i++) {
 			if ((i >= 10 && i % 10 != num && i / 10 != num) || (i < 10 && i % 10 != num)){
 				System.out.print(i + "\t");
+				this.numberList[count] = i;
 				count += 1;
 				if (count % 6 == 0) {
 					System.out.println("");
@@ -42,6 +43,33 @@ public class Lotto2 {
 			}
 		}
 		System.out.println("總共有" + count + "個數字可選");
+		System.out.println("");
+	}
+	
+	public void printBetNumber(int[] list) {
+		// the index of the total number list is 0 ~ 44
+		int count = 0; 
+		int[] betNumber = new int[6];
+		
+		while (count < 6) {			
+			int chosenNum = (int)(Math.random() * list.length); // get index of the number list elements
+			
+			if (list[chosenNum] != 0) {
+			// not 0 means it isn't a default value of array element, so keep testing it's repeat or not
+				for (int i = 0; i <= betNumber.length - 1; i++) {
+					if (betNumber[i] == list[chosenNum]) {
+						break;
+					} else if (i == 5) {
+						betNumber[count] = list[chosenNum];
+						count += 1;
+					}
+				}
+			}
+		}
+		System.out.println("六個樂透號碼：");
+		for (int i = 0; i <= 5; i++) {
+			System.out.print(betNumber[i] + "\t");
+		}
 	}
 	
 }
