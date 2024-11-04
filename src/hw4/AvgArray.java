9686f3f6110332aa9efd7b777f6f25b16d359321
Ariel Chien <jacinth170625@gmail.com>
2024-11-04 12:50:11 +0800

    modify AvgArray with constructor

--- src/hw4/AvgArray.java
+++ src/hw4/AvgArray.java
@@ -4,12 +4,16 @@ package hw4;
 //請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
 
 public class AvgArray {
-	private int[] arr;
+	private int[] arr = null;
 	private double avg;
+	
+	public AvgArray(int[] arr_parm){
+		this.arr = arr_parm;
+	}
 
-	public static void main(String[] args) {
-		AvgArray nums = new AvgArray();
+	public static void main(String[] args) {		
 		int a[] = {29, 100, 39, 41, 50, 8, 66, 77, 95, 15};
+		AvgArray nums = new AvgArray(a);
 		nums.setArray(a);
 		nums.calcAvg(nums.getArray());
 		nums.printGreaterNums(nums.getArray(), nums.avg);
