b6914e93475a2d5d7e43da9d19c0519353f20ed4
Ariel Chien <jacinth170625@gmail.com>
2024-11-27 16:51:57 +0800

    feat: add function to double-check if input date is valid.

--- src/hw10/DateFormatter.java
+++ src/hw10/DateFormatter.java
@@ -6,7 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 //請設計一隻程式，讓使用者輸入日期(年月日，例如:20110131)後，可以轉成想要的輸出格式化成
-
 //(1)年/月/日(2)月/日/年(3)日/月/年三選一，而輸入非指定日期數字格式會顯示出提示訊息如圖
 
 public class DateFormatter {
